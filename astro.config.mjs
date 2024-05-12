4de7c402c1772ea15147eb8375a3b415ec3af7bb
andrzejsokolowski <odiihinia@gmail.com>
2024-05-12 23:26:50 +0200

    web 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel({
+    speedInsights: {
+      enabled: true,
+    },
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+  }),
+  integrations: [tailwind(), mdx(), sitemap()],
+  site: 'https://www.ihh.dev',
+  redirects: {
+    '/contact': 'https:///www.linkedin.com/in/ansok99/',
+    '/cv': '/cv.pdf',
+    '/resume': '/resume.pdf'
+  },
+})
